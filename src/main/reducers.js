dd926bf08cd6fae96ae81d8b77cc9a2456761d0e
Vinicius Puente <viniciuspuente2@gmail.com>
2019-06-27 11:46:53 -0300

    - create reducers - create router - create main app file - add index file

---
+++ src/main/reducers.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
+
+import DashboardReducer from '../dashboard/dashboardReducer'
+
+const rootReducer = combineReducers({
+    dashboard: DashboardReducer,
+    form: formReducer,
+    toastr: toastrReducer
+})
+
+export default rootReducer
